44ad7406c29a80614773d680016b0666a470e11f
immxmmi <immxmmi@gmx.at>
2022-01-07 18:49:21 +0100

    Socket erstellt

---
+++ src/at/technikum/server/socket/Socket.java
@@ -0,0 +1,31 @@
+package at.technikum.server.socket;
+
+import lombok.Getter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Socket implements ISocket {
+
+    // Socket
+    @Getter
+    java.net.Socket socket;
+
+    public Socket(java.net.Socket socket) {
+        this.socket = socket;
+    }
+
+    // Input Stream
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return socket.getInputStream();
+    }
+
+    // Output Stream
+    @Override
+    public OutputStream getOutputStream() throws IOException {
+        return socket.getOutputStream();
+    }
+
+}
