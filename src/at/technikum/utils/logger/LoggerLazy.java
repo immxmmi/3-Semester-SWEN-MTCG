6635de37eb0a027b20455859f4ffac59b09de6fe
immxmmi <immxmmi@gmx.at>
2022-01-07 22:41:32 +0100

    fertig

---
+++ src/at/technikum/utils/logger/LoggerLazy.java
@@ -0,0 +1,39 @@
+package at.technikum.utils.logger;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LoggerLazy {
+
+    private Logger logData;
+
+    private LoggerLazy() {
+        FileHandler history = null;
+        try {
+            history = new FileHandler("history.log", true);
+            SimpleFormatter formatter = new SimpleFormatter();
+            history.setFormatter(formatter);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        logData = Logger.getLogger(LoggerStatic.class.getName());
+        logData.addHandler(history);
+    }
+
+    public void log(String logMessage) {
+        logData.log(Level.INFO, logMessage);
+    }
+
+
+    public static LoggerLazy getInstance() {
+        return LazyHolder.instance;
+    }
+
+    private static class LazyHolder {
+        private static final LoggerLazy instance = new LoggerLazy();
+    }
+}
