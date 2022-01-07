6156a752da5086ac538092ae60b674c7a4b4d690
immxmmi <immxmmi@gmx.at>
2022-01-07 22:46:35 +0100

    fertig

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import at.technikum.server.Server;
+import at.technikum.utils.logger.LoggerStatic;
 
 public class Main {
 
     public static void main(String[] args) {
-        Thread service = new Thread((Runnable) new Server(10003, 5, "localhost"));
+        LoggerStatic loggerStatic = LoggerStatic.getInstance();
+        loggerStatic.log("\nStart Game\n");
+
+        Thread service = new Thread((Runnable) new Server(10002, 5, "localhost"));
         service.start();
     }
 }
