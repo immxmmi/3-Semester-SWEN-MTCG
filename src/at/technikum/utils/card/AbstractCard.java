0ae60937a7d3124072da3c0097216df4d33357c6
immxmmi <immxmmi@gmx.at>
2022-01-07 20:23:33 +0100

    Karten erstellt -fix lib

--- src/at/technikum/utils/card/AbstractCard.java
+++ src/at/technikum/utils/card/AbstractCard.java
@@ -8,7 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import com.google.gson.annotations.SerializedName;
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class AbstractCard extends Tools implements ICard {
